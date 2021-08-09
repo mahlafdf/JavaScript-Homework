592e3c8fa7750f05e4cce418e9ecae9a035fed11
mahlafadaei2021@gmail.com <mahlafadaei2021@gmail.com>
2021-08-09 10:31:55 +0430

    ex-1

---
+++ loghello.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var sentence= ['hello,world!','Hola','नमस्ते ','Oi','Здравствуйте','안녕하십니까','Hallo','Merhaba','你好','سلام']
+
+
+console.log(sentence[0])
+console.log(sentence[1])
+console.log(sentence[2])
+console.log(sentence[3])
+console.log(sentence[4])
+console.log(sentence[5])
+console.log(sentence[6])
+console.log(sentence[7])
+console.log(sentence[8])
+console.log(sentence[9])
+console.log(sentence[10])
