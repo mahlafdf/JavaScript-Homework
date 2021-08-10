771f819cf91757fadabb749336993dbc7c26b370
Mahla Fadaei <mahlafadaei2021@gmail.com>
2021-08-10 15:39:03 +0430

    Create EX-6 Log an array of animals.js

---
+++ EX-6 Log an array of animals.js
@@ -0,0 +1,9 @@
+var items = [] ;
+console.log("This is an array");
+console.log (items);
+
+var animals =[ 'mouse','cat','dog'];
+console.log(animals);
+
+animals.push('piglet');
+console.log(animals);
