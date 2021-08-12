2ac3f8aabdc764d4f441f36dc6d9447fc60f2aa5
Mahla Fadaei <mahlafadaei2021@gmail.com>
2021-08-12 13:09:57 +0430

    Update Ex10-Comparearrays.js

--- Ex10-Comparearrays.js
+++ Ex10-Comparearrays.js
@@ -3,3 +3,9 @@ let y = [10, 'ten', 24, 'bye'];
 
 console.log('The length of the array is' + x.length);
 console.log('The length of the array is' + y.length);
+
+if( x.length == y.lenght){
+  console.log( 'The same size');
+} else {
+  console.log('Not the same');
+}
