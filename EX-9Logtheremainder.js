e5af30eb332b5afe3ee07ea306bce46420e8f74e
Mahla Fadaei <mahlafadaei2021@gmail.com>
2021-08-12 13:01:36 +0430

    Create EX-9Logtheremainder.js

---
+++ EX-9Logtheremainder.js
@@ -0,0 +1,15 @@
+/* If x equals 7, and the only other statement is x = x % 3, what would be the value of x after the calculation?
+
+my answer is :1 
+
+If y equals 21, and the only other statement is y = y % 4, what would be the value of y after the calculation?
+
+my answer is :1
+
+
+If z equals 13, and the only other statement is z = z % 2, what would be the value of z after the calculation?
+
+my answer is : 1 
+
+*/
+
