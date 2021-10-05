1f89c5cbfe90aed93950d1e6142e50e05ca7a988
mahlafadaei2021@gmail.com <mahlafadaei2021@gmail.com>
2021-10-05 18:18:54 +0330

    added ex2

---
+++ week3/ex2-dogYears.js
@@ -0,0 +1,9 @@
+function calculateDogAge (ageNumber){
+    let  calcAge = 7*ageNumber ;
+    return `Your doggie is ${calcAge} years old in dog years!`;
+}
+
+console.log(calculateDogAge(1));
+console.log(calculateDogAge(2));
+console.log(calculateDogAge(13));
+console.log(calculateDogAge(10));
